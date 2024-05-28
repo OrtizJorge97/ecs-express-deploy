6d2dea2aab234c57f70a6b30cebe6b11817d6c96
OrtizJorge97 <jorge.ortiz.c97@gmail.com>
2024-05-28 13:52:21 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ecs-express-deploy
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+// Define a health check endpoint
+app.use('/health', (req, res) => {
+  res.status(200).send('OK'); // Respond with a status code of 200 and 'OK'
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Start the server on a specific port (e.g., 8080)
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
